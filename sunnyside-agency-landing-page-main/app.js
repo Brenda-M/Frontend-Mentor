b4229df39ee17b0aae8e758951e3d4582e4f5478
brenda-m <brendanjuguna1@gmail.com>
2023-12-17 22:42:50 +0300

    add active states

--- sunnyside-agency-landing-page-main/app.js
+++ sunnyside-agency-landing-page-main/app.js
@@ -1,8 +1,26 @@
-const hamburgerMenu = document.getElementById('hamburger-menu');
+document.addEventListener("DOMContentLoaded", function() {
+  
+  const hamburgerMenu = document.getElementById('hamburger-menu');
 
 const mobileMenuContainer = document.getElementById('mobile-menu-container');
+const desktopNav = document.querySelector('.nav-items');
 
   hamburgerMenu.addEventListener('click', function () {
     mobileMenuContainer.style.display =
       mobileMenuContainer.style.display === 'block' ? 'none' : 'block';
   });
+
+  function handleScreenSizeChange() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      desktopNav.style.display = "none";
+    } else {
+      mobileMenuContainer.style.display = "none";
+    }
+  }
+
+
+  handleScreenSizeChange();
+
+  window.matchMedia("(max-width: 768px)").addListener(handleScreenSizeChange);
+
+})
